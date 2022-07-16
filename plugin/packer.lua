a00a77bb60a68824b838efc39f9254f89d8331c8
kshshton <96781803+kshshton@users.noreply.github.com>
2022-07-16 13:20:15 +0200

    Add treesitter

--- plugin/packer.lua
+++ plugin/packer.lua
@@ -17,6 +17,10 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
 
   if packer_bootstrap then
     require('packer').sync()
